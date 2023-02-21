9b0363f2a56e496d18d2310a6c05fa87d498a052
Henrique Venchiarutti <henrique.venchiarutti@webjump.com.br>
2023-02-21 19:06:43 -0300

    1 - Criação de Controller, e GraphQLs

---
+++ src/app/code/Estudo/QuickLearnGraph/Model/Mapper/CustomerDataMapper.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Estudo\QuickLearnGraphQL\Model\Mapper;
+
+use Magento\Customer\Api\Data\CustomerInterface;
+
+/**
+ * Class to map customer data
+ */
+class CustomerDataMapper
+{
+    /**
+     * @var string
+     */
+    public const EMAIL_ARG = 'email';
+
+    /**
+     * @var string
+     */
+    public const NAME_ARG = 'name';
+
+    /**
+     * @var string
+     */
+    public const DOB_ARG = 'dob';
+
+    /**
+     * Map customer data to an array
+     *
+     * @param CustomerInterface $customer
+     * @return array
+     */
+    public function map(CustomerInterface $customer): array
+    {
+        $dateTime = \DateTime::createFromFormat('Y-m-d', $customer->getDob() ?? "");
+
+        return [
+            self::EMAIL_ARG => $customer->getEmail(),
+            self::NAME_ARG => $customer->getFirstname() . " " .  $customer->getLastname(),
+            self::DOB_ARG => $dateTime ? $dateTime->format('d/m/Y') : "Usuário não possui DOB cadastrada"
+        ];
+    }
+}
---
+++ src/app/code/Estudo/QuickLearnGraphQL/Model/Service/UpdateCustomerDataService.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace Estudo\QuickLearnGraphQL\Model\Service;
+
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\State\InputMismatchException;
+
+/**
+ * Class to update customer data
+ */
+class UpdateCustomerDataService
+{
+    /**
+     * @var string
+     */
+    public const FIRSTNAME_ARG = 'firstname';
+
+    /**
+     * @var string
+     */
+    public const DOB_ARG = 'dob';
+
+    private CustomerRepositoryInterface $customerRepository;
+
+    /**
+     * @param CustomerRepositoryInterface $customerRepository
+     */
+    public function __construct(
+        CustomerRepositoryInterface $customerRepository
+    ) {
+        $this->customerRepository = $customerRepository;
+    }
+
+    /**
+     * Update customer data
+     *
+     * @param CustomerInterface $customer
+     * @param array $data
+     * @return CustomerInterface
+     * @throws InputException
+     * @throws LocalizedException
+     * @throws InputMismatchException
+     */
+    public function updateCustomerData(CustomerInterface $customer, array $data): CustomerInterface
+    {
+        if ($data[self::DOB_ARG]) {
+            $customer->setDob($data[self::DOB_ARG]);
+        }
+
+        if ($data[self::FIRSTNAME_ARG]) {
+            $customer->setFirstname($data[self::FIRSTNAME_ARG]);
+        }
+
+        return $this->customerRepository->save($customer);
+    }
+}
